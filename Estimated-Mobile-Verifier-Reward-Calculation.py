8cee74073ae1b96bdff47601dacee0702750822a
nikitasattarov <Nikita09082000@gmail.com>
2025-06-30 14:52:05 +0400

    some updates

--- Estimated-Mobile-Verifier-Reward-Calculation.py
+++ Estimated-Mobile-Verifier-Reward-Calculation.py
@@ -20,10 +20,10 @@ new_mamaboard_list = [1500, 5000, 5000, 7500, 20000, 25000, 30000, 40000, 45000,
                     2000, 3000, 10000, 10000, 15000, 40000, 50000, 60000, 80000, 90000, 100000, 160000, 180000, 240000, \
                     12000, 40000, 40000, 60000, 160000, 200000, 240000, 320000, 360000, 400000, 640000, 720000, 960000]
 
+st.title("Estimated Mobile Verifier Reward Calculation")
 
-
-node_type_option = st.selectbox(
-    "Select the type of node:",
+max_mamaboard_comp = st.selectbox(
+    "Select the most recently purchased Mamaboard component:",
     (
         "Battery Power",
         "Network Card",
@@ -42,6 +42,15 @@ node_type_option = st.selectbox(
     index=0
 )
 
+max_mamaboard_lvl = st.selectbox(
+    "Select the level of the most recently purchased Mamaboard component:",
+    (
+        1,
+        2,
+        3
+    ),
+    index=0
+)
 
 
 
