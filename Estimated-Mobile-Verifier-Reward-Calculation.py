7074afa51a6fafd91947edb744aba7a4086a2e5f
nikitasattarov <Nikita09082000@gmail.com>
2025-06-30 15:11:45 +0400

    some updates

--- Estimated-Mobile-Verifier-Reward-Calculation.py
+++ Estimated-Mobile-Verifier-Reward-Calculation.py
@@ -81,6 +81,6 @@ if max_mamaboard_lvl == 2:
     st.write(sublist_max_mamaboard_component_index)
 
 if max_mamaboard_lvl == 3:
-    sublist_max_mamaboard_component_index = max_mamaboard_component_index_list[:-13]
+    sublist_max_mamaboard_component_index = max_mamaboard_component_index_list[-13:]
     st.write(sublist_max_mamaboard_component_index)
 
